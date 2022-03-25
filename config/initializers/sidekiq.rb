3219e4ffa9e2eebc55c800d4306d9ad686b89831
Eddy Almuiña <almuina.e@gmail.com>
2022-03-25 13:43:32 +0100

    Listening to green/record and storing records in DB

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,4 +6,13 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = sidekiq_config
+
+  schedule_file = 'config/schedule.yml'
+  if File.exist?(schedule_file) && Rails.env.production?
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
+
+  Rails.application.config.after_initialize do
+    MqttJob.perform_async
+  end
 end
---
+++ test/sidekiq/mqtt_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MqttJobTest < Minitest::Test
+  def test_example
+    skip "add some examples to (or delete) #{__FILE__}"
+  end
+end
