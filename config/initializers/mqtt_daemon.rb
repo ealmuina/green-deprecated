42c23aa64f97a0156481a9ea15254bbf73d4e6c2
Eddy Almuiña <almuina.e@gmail.com>
2022-03-26 14:35:21 +0100

    Mqtt runner (#18)
    * KISS: Removed redis and sidekiq. Using a simpler thread approach instead for running the MQTT daemon
    
    * Do not launch MQTT daemon when testing

--- config/initializers/mqtt_daemon.rb
+++ config/initializers/mqtt_daemon.rb
@@ -1,27 +1,29 @@
 require 'json'
 require 'paho-mqtt'
 
-### Create a simple client with default attributes
-client = PahoMqtt::Client.new
+unless ENV['RAILS_ENV'] == 'test'
+  ### Create a simple client with default attributes
+  client = PahoMqtt::Client.new
 
-### Callbacks
-client.on_message do |message|
-  payload = JSON.parse(message.payload)
+  ### Callbacks
+  client.on_message do |message|
+    payload = JSON.parse(message.payload)
 
-  if message.topic == 'green/record'
-    node = Node.find_by(chip_id: payload['node_id'])
+    if message.topic == 'green/record'
+      node = Node.find_by(chip_id: payload['node_id'])
 
-    if node
-      Record.create(
-        node: node,
-        moisture: payload['moisture']
-      )
+      if node
+        Record.create(
+          node: node,
+          moisture: payload['moisture']
+        )
+      end
     end
   end
+
+  ### Connect to the MQTT server
+  client.connect(ENV["MQTT_BROKER_HOST"], ENV["MQTT_BROKER_PORT"])
+
+  ### Subscribe to topics
+  client.subscribe("green/record", 2)
 end
-
-### Connect to the MQTT server
-client.connect(ENV["MQTT_BROKER_HOST"], ENV["MQTT_BROKER_PORT"])
-
-### Subscribe to topics
-client.subscribe("green/record", 2)
